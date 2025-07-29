18618e99893725d0ff3b7c78211356e7b81db539
Anders Englund <a.englund@leinelinde.se>
2025-07-29 14:14:32 +0200

    Using secret file for network authentications

--- src/wifi.ino
+++ src/wifi.ino
@@ -3,12 +3,28 @@
 
 ESP8266WiFiMulti wifiMulti;
 
+typedef struct {
+  const char *network;
+  const char *password;
+} Authentication_t;
+
+Authentication_t authentications[] = {
+  // Below file contains the network ssid and passwords of the
+  // networks that the chip can connect to. It should be kept
+  // secret and not added to version control.
+#include "authentications.secret"
+};
+
 void wifi_setup() {
-  // add Wi-Fi networks you want to connect to
-  wifiMulti.addAP("network1", "password1");
-  wifiMulti.addAP("network2", "password2");
-  wifiMulti.addAP("network3", "password3");
-  wifiMulti.addAP("network4", "password4");
+
+  int n = sizeof(authentications) / sizeof(Authentication_t);
+  if (n == 0) {
+    return; // No networks configured
+  }
+
+  for (int i = 0; i < n; i++) {
+    wifiMulti.addAP(authentications[i].network, authentications[i].password);
+  }
 
   while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
     delay(250);
